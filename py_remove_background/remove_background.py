bd1be3d657e535ce2d97741636ec5b9b91ec4845
mammadsafar <mohammadsafar2014@gmail.com>
2022-11-30 23:30:03 +0330

    first commit

---
+++ py_remove_background/remove_background.py
@@ -0,0 +1,11 @@
+# Qiftolly.ir
+# pip install rembg[gpu]
+from rembg import remove
+from PIL import Image
+
+input_path = 'input.png'
+output_path = 'output.png'
+
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
